6cb08140bb7138a530443d3aba76054301643ddd
Pooria Attarzadeh <pooria_az@ymail.com>
2015-04-12 23:36:48 +0430

    data loaded from JSON

--- js/controllers.js
+++ js/controllers.js
@@ -1,6 +1,7 @@
 var cchopApp = angular.module('cchopApp',[]);
 
-cchopApp.controller('FrontpageCtrl', ['$scope' ,'$http', function($scope,$http) {
+cchopApp.controller('FrontpageCtrl', ['$scope' ,'$http',
+ function($scope,$http) {
 	$http.get('data/frontpage.json').success(function(data){
 		$scope.data = data;
 	});
