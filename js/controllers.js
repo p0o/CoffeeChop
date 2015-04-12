aaf3e293039cfff13854c27e814e44cc2f16dc25
Pooria Attarzadeh <pooria_az@ymail.com>
2015-04-12 22:49:04 +0430

    angular coding

--- js/controllers.js
+++ js/controllers.js
@@ -1,5 +1,7 @@
 var cchopApp = angular.module('cchopApp',[]);
 
-cchopApp.controller('MainPageCtrl', ['$scope' , function($scope) {
-
+cchopApp.controller('FrontpageCtrl', ['$scope' ,'$http', function($scope,$http) {
+	$http.get('data/frontpage.json').success(function(data){
+		$scope.data = data;
+	});
 }]);
