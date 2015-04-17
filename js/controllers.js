8acfe20a60255578e9a6d655fbb224b2f10282d2
Pooria Attarzadeh <pooria_az@ymail.com>
2015-04-17 22:41:32 +0430

    Discount colculator is added

--- js/controllers.js
+++ js/controllers.js
@@ -18,6 +18,11 @@ cchopControllers.controller('ViewitemCtrl',['$scope','$http','$routeParams','ite
  	$scope.sizeBtnToggle = function(size) {
  		$scope.sizeBtn[size] = !$scope.sizeBtn[size];
  	}
+ 	// Color selector
+ 	$scope.colorBtn = {};
+ 	$scope.colorBtnToggle = function(color) {
+ 		$scope.colorBtn[color] = !$scope.colorBtn[color];
+ 	}
  	// Creating item feed URL to fetch data
  	url = feedTemplateURL.replace('[PRODUCTID]',$routeParams.itemId);
  	$http.get(url).success(function(data) {
@@ -25,6 +30,20 @@ cchopControllers.controller('ViewitemCtrl',['$scope','$http','$routeParams','ite
  		// Seperate sizes and colors string to Arrays
  		$scope.item.sizes = data.available_sizes.split('|');
  		$scope.item.colors = data.available_colors.split('|');
+ 		// Calculate discount if it's not zero
+ 		data.off = parseInt(data.off);
+ 		data.price = parseFloat(data.price);
+ 		if (data.off !== 0) 
+ 			$scope.hasOff = true;
+
+ 		$scope.getFinalPrice = function() {
+ 			if ($scope.hasOff) {
+ 				var price = data.price * (1 - (data.off/100));
+ 				return price.toFixed(2);
+ 			}
+ 			else
+ 				return data.price;
+ 		}	
  	});
 }]);
 
